6933b98294afdb41e58d22754e935107d2cce7c9
Zaka Elab <zakaelab@gmail.com>
2015-10-16 19:20:36 +0200

    Added P and I
    The controller know have the ability to do proportional and integral
    correction to the rotation, not to the speed. The speed is controlled
    with a simple condition: if the distance is lower than a threshold stop,
    if the distance is greater speed equals max velocity. It's a bit
    simplistic, but it works so far.

--- brain.py
+++ brain.py
@@ -1,59 +1,50 @@
 # robot goes forward and then slows to a stop when it detects something  
-   
+
+import datetime
 from pyrobot.brain import Brain  
    
-# class Avoid(Brain):  
-           
-#    # Give the front two sensors, decide the next move  
-#    def determineMove(self, front, left, right):  
-#       if front < 0.5:   
-#          #print "obstacle ahead, hard turn"  
-#          return(0, .3)  
-#       elif left < 0.8:
-#          #print "object detected on left, slow turn"
-#          return(0.1, -.3)  
-#       elif right < 0.8: 
-#          #print "object detected on right, slow turn" 
-#          return(0.1, .3)  
-#       else:  
-#          #print "clear"  
-#          return(0.5, 0.0) 
-      
-#    def step(self):  
-#       front = min([s.distance() for s in self.robot.range["front"]])
-#       left = min([s.distance() for s in self.robot.range["left-front"]])
-#       right = min([s.distance() for s in self.robot.range["right-front"]])
-#       translation, rotate = self.determineMove(front, left, right)  
-#       self.robot.move(translation, rotate)
-
-# def INIT(engine):  
-#    assert (engine.robot.requires("range-sensor") and
-#            engine.robot.requires("continuous-movement"))
-#    return Avoid('Avoid', engine)  
-
 class Controller(Brain):  
-           
-   # Give the front two sensors, decide the next move  
-   def determineMove(self, sonar0, sonar2, front):
-      if front < 0.3 or sonar2 < 0.4:
-         # Turn right
-         return(0, -.3)
-      elif sonar0 < 0.29:
-         #print "object detected on sonar0, slow turn"
-         return(0.1, -.3)  
-      elif sonar0 >= 0.29 and sonar0 <= 0.35: 
-         #print "object detected on right, slow turn" 
-         return(0.4, 0)  
-      else:  
-         #print "clear"  
-         return(0.1, 0.3) 
+
+   setValue = 0.4                      # Expected distance from wall
+   h = 0                               # Elapsed time between loop cycle
+   I = 0                               # Integrate value
+   P = 0                               # Proportional value
+   kP = 1                              # Proportional K
+   kI = 1                              # Proportional I
+   tMinusOne = datetime.datetime.now() # Previous time
+   iMinusOne = 0                       # Previous integrate value
+   
+   def determineMove(self, frontLeftAndZero, frontAll):
+      "First approach, only proportional correction"
+
+      if frontAll < 0.2:
+         speed = 0
+      else:
+         speed = 0.3
+         
+      error = frontLeftAndZero - self.setValue
+
+      print "frontLeftAndZero:", frontLeftAndZero
+      print "speed:", speed
       
+      self.h = (datetime.datetime.now() - self.tMinusOne).total_seconds()
+      self.tMinusOne = datetime.datetime.now()
+      
+      # Proportional
+      self.P = self.kP*error
+
+      self.I = self.iMinusOne + self.kI*self.h*error
+      
+      rotation = self.P + self.I
+      
+      return (speed, rotation)
+   
    def step(self):  
-      sonar0 = self.robot.sonar[0][0].value
-      sonar2 = self.robot.sonar[0][2].value
-      front = min([s.distance() for s in self.robot.range["front"]])
+      frontLeftAndZero = min(self.robot.sonar[0][0].value,
+                 min([s.distance() for s in self.robot.range["front-left"]]))
+      frontAll = min([s.distance() for s in self.robot.range["front-all"]])
       
-      translation, rotate = self.determineMove(sonar0, sonar2, front)  
+      translation, rotate = self.determineMove(frontLeftAndZero, frontAll)  
       self.robot.move(translation, rotate)
 
 def INIT(engine):  
