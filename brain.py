c7f69dd5ea9be00bec8a74102ea2571a2cf461c8
Zaka Elab <zakaelab@gmail.com>
2015-10-17 23:37:14 +0200

    Add SimpleWorld which contains a simple box

--- brain.py
+++ brain.py
@@ -6,42 +6,60 @@ from pyrobot.brain import Brain
 class Controller(Brain):  
 
    setValue = 0.4                      # Expected distance from wall
-   h = 0                               # Elapsed time between loop cycle
-   I = 0                               # Integrate value
-   P = 0                               # Proportional value
-   kP = 1                              # Proportional K
-   kI = 1                              # Proportional I
+   h = datetime.datetime.now()                               # Elapsed time between loop cycle
+   kP = 5                              # Proportional K
+   kI = 2                              # Proportional K
+   kD = 1.5                              # Derivative K
    tMinusOne = datetime.datetime.now() # Previous time
-   iMinusOne = 0                       # Previous integrate value
+   iMinusOne = 0                       # Previous integral value
+   dMinusOne = 0                       # Previous derivative value
    
    def determineMove(self, frontLeftAndZero, frontAll):
-      "First approach, only proportional correction"
 
-      if frontAll < 0.2:
+      if frontAll < 0.3:
          speed = 0
       else:
-         speed = 0.3
-         
+         speed = 0.4
+
+      # speed = 0.3
+      
       error = frontLeftAndZero - self.setValue
 
-      print "frontLeftAndZero:", frontLeftAndZero
-      print "speed:", speed
-      
-      self.h = (datetime.datetime.now() - self.tMinusOne).total_seconds()
-      self.tMinusOne = datetime.datetime.now()
+      now = datetime.datetime.now()
+      self.h = (now - self.tMinusOne).total_seconds()
+      self.tMinusOne = now
       
       # Proportional
-      self.P = self.kP*error
+      P = self.kP * error
+      # Integral
+      I = self.iMinusOne + self.kI*self.h*error
+      # print "iMinusOne:", self.iMinusOne
+      self.iMinusOne = I
+      # Derivative
+      D = self.kD * (error - self.dMinusOne)/self.h
+      # print "dMinusOne:", self.dMinusOne
+      self.dMinusOne = error
+      rotation = P + I + D
 
-      self.I = self.iMinusOne + self.kI*self.h*error
-      
-      rotation = self.P + self.I
+      # # DEBUG
+      # print "Kp:", self.kP
+      # print "Ki:", self.kI
+      # print "Kd:", self.kD
+      # print "h:", self.h
+      # print "error:", error
+      # print "frontLeftAndZero:", frontLeftAndZero
+      # print "speed:", speed
+      # print "P:", P
+      # print "I:", I
+      # print "D:", D
+      # print "rotation:", rotation
+      # print
       
       return (speed, rotation)
    
    def step(self):  
-      frontLeftAndZero = min(self.robot.sonar[0][0].value,
-                 min([s.distance() for s in self.robot.range["front-left"]]))
+      # frontLeftAndZero = min(self.robot.sonar[0][0].value,
+      #            min([s.distance() for s in self.robot.range["front-left"]]))
       frontAll = min([s.distance() for s in self.robot.range["front-all"]])
       
       translation, rotate = self.determineMove(frontLeftAndZero, frontAll)  
---
+++ simpleWorld.py
@@ -0,0 +1,69 @@
+"""
+Mundo de Zaka
+"""
+
+from pyrobot.simulators.pysim import *
+
+def INIT():
+    TkWorldWidth = TkWorldHeight = 600
+    TkOuterBoxWidth = TkOuterBoxHeight = 550
+    TkScale = 80
+    TkOffsetX = (TkWorldWidth - TkOuterBoxWidth)/2
+    TkOffsetY = TkWorldHeight - (TkWorldHeight - TkOuterBoxHeight)/2
+
+    print "(width, height)", (TkWorldWidth, TkWorldHeight)
+    print "(offset x, offset y)", (TkOffsetX, TkOffsetY)
+    
+    # (width, height), (offset x, offset y), scale:
+    # sim = TkSimulator((446,491),(21,451),80.517190)
+    sim = TkSimulator((TkWorldWidth,TkWorldHeight),
+                      (TkOffsetX, TkOffsetY),
+                      TkScale)
+    
+    # x1, y1, x2, y2 in meters:
+    sim.addBox(0, 0, 7, 7)
+
+    # sim.addWall(2, 0, 2, 4, color="black")
+    # sim.addWall(2, 4, 1, 4, color="black")
+    # sim.addWall(1, 4, 1, 6, color="black")
+    # sim.addWall(1, 6, 2, 6, color="black")
+    # sim.addWall(2, 6, 2, 7, color="black")
+    # sim.addWall(0.2, 7, 0.2, 0, color="black")
+    
+    # sim.addWall(4.5, 7, 5, 6.2, color="black")
+    # sim.addWall(5, 6.2, 6, 6, color="black")
+    # sim.addWall(6, 6, 6, 5.8, color="black")
+    # sim.addWall(6, 5.8, 5.5, 5.8, color="black")
+    # sim.addWall(5.5, 5.8, 5.4, 5.2, color="black")
+    # sim.addWall(5.4, 5.2, 6.3, 2.1, color="black")
+    # sim.addWall(6.3, 2.1, 5.6, 1.2, color="black")
+    # sim.addWall(5.6, 1.2, 5.6, 0.8, color="black")
+    # sim.addWall(5.6, 0.8, 6, 0.4, color="black")
+    # sim.addWall(6, 0.4, 6, 0, color="black")
+    # sim.addWall(6, 0, 7, 0, color="black")
+    # sim.addWall(0, 7, 7, 7, color="black")
+    # sim.addWall(7, 7, 4.5, 7, color="black")
+    
+    # port, name, x, y, th, bounding Xs, bounding Ys, color
+    # (optional TK color name):
+    # sim.addRobot(60000, TkPioneer("RedPioneer",
+    #                               .5, 2.5, 0.00,
+    #                               ((.225, .225, -.225, -.225),
+    #                                (.175, -.175, -.175, .175))))
+    sim.addRobot(60000, TkPioneer("RedPioneer",
+                                  2.5, 0.5, 0.00,
+                                  ((.225, .225, -.225, -.225),
+                                   (.175, -.175, -.175, .175))))
+
+    
+    # add some sensors:
+    # sim.robots[0].addDevice(PioneerFrontSonars()) # for 8 front sonar
+    sim.robots[0].addDevice(Pioneer16Sonars()) # for full 360 sonar
+    sim.robots[0].addDevice(PioneerFrontLightSensors())
+    # x, y relative to body center (beyond bounding box):
+    sim.robots[0].addDevice(BulbDevice(0.226, 0)) # pose x, pose y
+    # width, height, startAngle, stopAngle, pose x, pose y, pose thr:
+    # cam = Camera(60, 40, 0, 120, 0, 0, 0)
+    # sim.robots[0].addDevice(cam)
+    # sim.robots[0].addDevice( PTZ(cam) )
+    return sim
