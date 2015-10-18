5d80a133035c51f5d72e2804e6ed1637328bd037
Zaka Elab <zakaelab@gmail.com>
2015-10-18 03:21:02 +0200

    Robot turns right when sensor[3] > sensor[4].
    Also isolated variables used only by determineRotation and add them to
    its scope only.

--- brain.py
+++ brain.py
@@ -1,48 +1,47 @@
-# robot goes forward and then slows to a stop when it detects something  
-
 import datetime
 from pyrobot.brain import Brain  
-   
+
+# TODO: Explore the possibility of a smooth turn left
+
 class Controller(Brain):  
 
    expectedDistance = 0.4              # Expected distance from wall
-   h = datetime.datetime.now() # Elapsed time between loop cycle
-   # h = 1                               # Elapsed time between loop cycle
-   kP = 5                              # Proportional K
-   kI = .00001                        # Proportional K
-   kD = .00001                        # Derivative K
-   Ti = kP/float(kI)
-   Td = kD/float(kP)
-
+   h = datetime.datetime.now()         # Elapsed time between loop cycle
    prevDistance = expectedDistance
    tMinusOne = datetime.datetime.now() # Previous time
-   iMinusOne = 0                       # Previous integral value
-   dMinusOne = 0                       # Previous derivative value
-
+   
    def determineRotation(self, distanceToWall):
+      kP = 5            # Proportional K
+      kI = .00001       # Proportional K
+      kD = .00001       # Derivative K
+      Ti = kP/float(kI) # Integral (constant) time (period)
+      Td = kD/float(kP) # Derivative (constant) time (period)
+      iMinusOne = 0     # Previous integral value
+      dMinusOne = 0     # Previous derivative value
+
       error = distanceToWall - self.expectedDistance
       
+      # Proportional
+      P = kP * error
+      
+      # Integral
+      I = iMinusOne + kI*self.h*error
+      iMinusOne = I
+      
+      # Derivative
+      D = Td/(Td + self.h)*dMinusOne \
+          - kD/(Td + self.h)*(distanceToWall + self.prevDistance)
+
+      self.prevDistance = distanceToWall
+      dMinusOne = error
+      
+      return (P + I + D)
+
+   def determineMove(self, distance, frontAll):
       now = datetime.datetime.now()
       self.h = (now - self.tMinusOne).total_seconds()
       self.tMinusOne = now
-      
-      # Proportional
-      P = self.kP * error
-      
-      # Integral
-      I = self.iMinusOne + self.kI*self.h*error
-      self.iMinusOne = I
-      
-      # Derivative
-      D = self.Td/(self.Td + self.h)*self.dMinusOne \
-          - self.kD/(self.Td + self.h)*(distanceToWall + self.prevDistance)
 
-      self.prevDistance = distanceToWall
-      self.dMinusOne = error
-      
-      return (P + I + D)
-   
-   def determineMove(self, distance, frontAll):
       if frontAll < 0.3:
          speed = 0
       else:
@@ -53,8 +52,13 @@ class Controller(Brain):
    def step(self):  
       distance = min([s.distance() for s in self.robot.range["front-left"]])
       frontAll = min([s.distance() for s in self.robot.range["front-all"]])
-      
-      translation, rotate = self.determineMove(distance, frontAll)  
+
+      translation, rotate = self.determineMove(distance, frontAll)
+
+      if self.robot.sonar[0][3].distance() > self.robot.sonar[0][4].distance():
+         # Turn right a little
+         rotate = rotate - 0.4
+         
       self.robot.move(translation, rotate)
 
 def INIT(engine):  
