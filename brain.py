562e6b3bac31f6ca87eddd530d68fb2c3f3912e8
Zaka Elab <zakaelab@gmail.com>
2015-10-18 02:26:09 +0200

    Several changes in controller
    * 1 - Get rid of previous formulas and use the ones provided by Javier
    de Lope
    * 2 - Isolate code to get the PID value for rotation
    * 3 - Stumbled upon decent values for Kp, Ki and Kd.

--- brain.py
+++ brain.py
@@ -5,64 +5,56 @@ from pyrobot.brain import Brain
    
 class Controller(Brain):  
 
-   setValue = 0.4                      # Expected distance from wall
-   h = datetime.datetime.now()                               # Elapsed time between loop cycle
+   expectedDistance = 0.4              # Expected distance from wall
+   h = datetime.datetime.now() # Elapsed time between loop cycle
+   # h = 1                               # Elapsed time between loop cycle
    kP = 5                              # Proportional K
-   kI = 2                              # Proportional K
-   kD = 1.5                              # Derivative K
+   kI = .00001                        # Proportional K
+   kD = .00001                        # Derivative K
+   Ti = kP/float(kI)
+   Td = kD/float(kP)
+
+   prevDistance = expectedDistance
    tMinusOne = datetime.datetime.now() # Previous time
    iMinusOne = 0                       # Previous integral value
    dMinusOne = 0                       # Previous derivative value
-   
-   def determineMove(self, frontLeftAndZero, frontAll):
 
-      if frontAll < 0.3:
-         speed = 0
-      else:
-         speed = 0.4
-
-      # speed = 0.3
+   def determineRotation(self, distanceToWall):
+      error = distanceToWall - self.expectedDistance
       
-      error = frontLeftAndZero - self.setValue
-
       now = datetime.datetime.now()
       self.h = (now - self.tMinusOne).total_seconds()
       self.tMinusOne = now
       
       # Proportional
       P = self.kP * error
+      
       # Integral
       I = self.iMinusOne + self.kI*self.h*error
-      # print "iMinusOne:", self.iMinusOne
       self.iMinusOne = I
-      # Derivative
-      D = self.kD * (error - self.dMinusOne)/self.h
-      # print "dMinusOne:", self.dMinusOne
-      self.dMinusOne = error
-      rotation = P + I + D
-
-      # # DEBUG
-      # print "Kp:", self.kP
-      # print "Ki:", self.kI
-      # print "Kd:", self.kD
-      # print "h:", self.h
-      # print "error:", error
-      # print "frontLeftAndZero:", frontLeftAndZero
-      # print "speed:", speed
-      # print "P:", P
-      # print "I:", I
-      # print "D:", D
-      # print "rotation:", rotation
-      # print
       
-      return (speed, rotation)
+      # Derivative
+      D = self.Td/(self.Td + self.h)*self.dMinusOne \
+          - self.kD/(self.Td + self.h)*(distanceToWall + self.prevDistance)
+
+      self.prevDistance = distanceToWall
+      self.dMinusOne = error
+      
+      return (P + I + D)
+   
+   def determineMove(self, distance, frontAll):
+      if frontAll < 0.3:
+         speed = 0
+      else:
+         speed = 0.4
+
+      return (speed, self.determineRotation(distance))
    
    def step(self):  
-      # frontLeftAndZero = min(self.robot.sonar[0][0].value,
-      #            min([s.distance() for s in self.robot.range["front-left"]]))
+      distance = min([s.distance() for s in self.robot.range["front-left"]])
       frontAll = min([s.distance() for s in self.robot.range["front-all"]])
       
-      translation, rotate = self.determineMove(frontLeftAndZero, frontAll)  
+      translation, rotate = self.determineMove(distance, frontAll)  
       self.robot.move(translation, rotate)
 
 def INIT(engine):  
